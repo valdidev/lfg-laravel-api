3eb31f4e9c08e6257c23c6aca6a5a09bed345ef7
valdidev <valdicode.fernando@gmail.com>
2022-12-15 14:05:03 +0100

    feat: posts migration and model

---
+++ database/migrations/7_2022_12_15_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('message', 200);
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('party_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('party_id')->references('id')->on('parties');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
