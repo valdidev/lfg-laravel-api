365b04ad9f29e027b3348550f825e00ead483e76
valdidev <valdicode.fernando@gmail.com>
2022-12-15 14:05:29 +0100

    chore: more info user fixed

---
+++ app/Models/MoreUserInfo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MoreUserInfo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'surname',
+        'age',
+        'steam_account',
+    ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+}
